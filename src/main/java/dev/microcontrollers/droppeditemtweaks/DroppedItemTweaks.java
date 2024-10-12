67a7309584a335cd3e43fe8e24e0677cec4e656c
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-10-11 22:36:26 -0500

    1.2.0 multiloader and item config

--- src/main/java/dev/microcontrollers/droppeditemtweaks/DroppedItemTweaks.java
+++ src/main/java/dev/microcontrollers/droppeditemtweaks/DroppedItemTweaks.java
@@ -1,11 +1,29 @@
 package dev.microcontrollers.droppeditemtweaks;
 
 import dev.microcontrollers.droppeditemtweaks.config.DroppedItemTweaksConfig;
+//? if fabric
 import net.fabricmc.api.ModInitializer;
+//? if neoforge {
+/*import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+*///?}
 
-public class DroppedItemTweaks implements ModInitializer {
+//? if neoforge
+/*@Mod(value = "droppeditemtweaks", dist = Dist.CLIENT)*/
+public class DroppedItemTweaks /*? if fabric {*/ implements ModInitializer /*?}*/ {
+	//? if fabric {
 	@Override
 	public void onInitialize() {
 		DroppedItemTweaksConfig.CONFIG.load();
 	}
+	//?}
+
+	//? if neoforge {
+    /*public DroppedItemTweaks() {
+        DroppedItemTweaksConfig.CONFIG.load();
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (client, parent) -> DroppedItemTweaksConfig.configScreen(parent));
+    }
+	*///?}
 }
--- src/main/java/dev/microcontrollers/droppeditemtweaks/mixin/ItemEntityRendererMixin.java
+++ src/main/java/dev/microcontrollers/droppeditemtweaks/mixin/ItemEntityRendererMixin.java
@@ -2,16 +2,12 @@ package dev.microcontrollers.droppeditemtweaks.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.blaze3d.vertex.PoseStack;
 import dev.microcontrollers.droppeditemtweaks.config.DroppedItemTweaksConfig;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.ItemEntityRenderer;
-//#if MC <= 1.20.4
-//$$ import net.minecraft.item.ItemStack;
-//#endif
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ItemEntityRenderer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,32 +15,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntityRenderer.class)
 public class ItemEntityRendererMixin {
-    @ModifyExpressionValue(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;sin(F)F"))
+    @ModifyExpressionValue(method = "render(Lnet/minecraft/world/entity/item/ItemEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;sin(F)F"))
     private float makeItemStatic(float value) {
         return DroppedItemTweaksConfig.CONFIG.instance().staticItems ? -1.0f : value;
     }
 
-    @Inject(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;multiply(Lorg/joml/Quaternionf;)V"))
-    private void scaleItems(ItemEntity itemEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
-        ItemStack stack = itemEntity.getStack();
+    @Inject(method = "render(Lnet/minecraft/world/entity/item/ItemEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lorg/joml/Quaternionf;)V"))
+    private void scaleItems(ItemEntity entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
+        ItemStack stack = entity.getItem();
         float scale = DroppedItemTweaksConfig.CONFIG.instance().itemScale;
-        if (DroppedItemTweaksConfig.CONFIG.instance().uhcOverlay != 0F
-                && (stack.isOf(Items.APPLE)
-                || stack.isOf(Items.GOLDEN_APPLE)
-                || stack.isOf(Items.GOLD_INGOT)
-                || stack.isOf(Items.GOLD_NUGGET)
-                || stack.isOf(Items.PLAYER_HEAD)))
-           scale = DroppedItemTweaksConfig.CONFIG.instance().uhcOverlay;
-        matrixStack.scale(scale, scale, scale);
+        if (DroppedItemTweaksConfig.CONFIG.instance().uhcOverlay != 0F && DroppedItemTweaksConfig.CONFIG.instance().uchItemList.contains(stack.getItem()))
+            scale = DroppedItemTweaksConfig.CONFIG.instance().uhcOverlay;
+        poseStack.scale(scale, scale, scale);
     }
 
-    @ModifyReturnValue(method = "getRenderedAmount", at = @At("RETURN"))
-    //#if MC >= 1.20.6
-    private static int forceStackAmount(int original, int stackSize) {
-        return DroppedItemTweaksConfig.CONFIG.instance().dropStackCount != 0 ? Math.min(DroppedItemTweaksConfig.CONFIG.instance().dropStackCount, stackSize) : original;
-    //#else
-    //$$ private int forceStackAmount(int original, ItemStack stack) {
-    //$$    return DroppedItemTweaksConfig.CONFIG.instance().dropStackCount != 0 ? Math.min(DroppedItemTweaksConfig.CONFIG.instance().dropStackCount, stack.getCount()) : original;
-    //#endif
+    @ModifyReturnValue(method = /*? if >=1.20.6 {*/ "getRenderedAmount" /*?} else {*/ /*"getRenderAmount" *//*?}*/, at = @At("RETURN"))
+    private /*? if >=1.20.6 {*/ static /*?}*/ int forceStackAmount(int original, /*? if >=1.20.6 {*/ int stackSize /*?} else {*/ /*ItemStack stack *//*?}*/) {
+        return DroppedItemTweaksConfig.CONFIG.instance().dropStackCount != 0 ? Math.min(DroppedItemTweaksConfig.CONFIG.instance().dropStackCount, /*? if >=1.20.6 {*/ stackSize /*?} else {*/ /*stack.getCount() *//*?}*/) : original;
     }
 }
